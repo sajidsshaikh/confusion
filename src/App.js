68a6933c0e3ee79c9312172d2b19e92b164cf006
Sajid Shaikh <sajid9379@gmail.com>
2019-09-03 19:42:31 -0400

    Configuring Bootstrap for React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Wow that is fast
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Honk Kong University
-        </a>
-      </header>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <NavbarBrand href="/">Home</NavbarBrand>
+          <NavbarBrand href="/">Home2</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
   );
 }
