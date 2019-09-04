eddeda390829f097710a7ec7d1c55bd576fafc3c
Sajid Shaikh <sajid9379@gmail.com>
2019-09-04 14:38:26 -0400

    Commit React component part 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import logo from './logo.svg';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
@@ -13,6 +14,7 @@ function App() {
           <NavbarBrand href="/">Home2</NavbarBrand>
         </div>
       </Navbar>
+      <Menu></Menu>
     </div>
   );
 }
