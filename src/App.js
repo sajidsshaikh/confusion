d2acaf842aa1e08f7ffcd37418b472dfce0196cd
Sajid Shaikh <sajid9379@gmail.com>
2019-09-04 16:20:10 -0400

    Componets part 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import logo from './logo.svg';
 import Menu from './components/MenuComponent';
 import './App.css';
+import Dishes from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          <NavbarBrand href="/">Home</NavbarBrand>
-          <NavbarBrand href="/">Home2</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu></Menu>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      dishes : Dishes
+    };
+  }
+  render () {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/">Home</NavbarBrand>
+            <NavbarBrand href="/">Home2</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}></Menu>
+      </div>
+    );
+  }
 }
 
 export default App;
